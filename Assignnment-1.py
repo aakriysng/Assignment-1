e7fd4b2cea6e198526561d9c3862a2c4725cab70
aakriysng <aakritsingh9105@gmail.com>
2025-07-04 20:11:51 +0530

    Initial commit

---
+++ Assignnment-1.py
@@ -0,0 +1,24 @@
+#TAsk-->1
+a= int(input("enter the number a: "))
+b= int(input("enter the number b: "))
+
+addition= a+b
+subtraction= a-b
+multiplication= a*b
+division= a/b
+
+print("addition: ",a+b)
+print("subtraction: ",a-b)
+print("multiplication: ",a*b)
+print("divison: ",a/b)
+
+
+#task-->2
+
+first_name= input("enter the first_name: ")
+last_name= input("enter the last_name: ")
+
+
+full_name= first_name + " " + last_name
+
+print(f"Hello, {full_name}! Welcome to the Python program.")
